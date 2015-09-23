158084e98bb8f64c3ad6c272ae784bf9d3e30116
Trenton <tjboyette@gmail.com>
2015-09-23 17:22:02 -0400

    added weather

--- weather.py
+++ weather.py
@@ -6,14 +6,16 @@ print('''This function will produce for you the weather conditions for your area
 
 print
 
-location = input("ZIP code? ")
+zip = input("ZIP code? ")
 
 import urllib2
 import json
-f = urllib2.urlopen('http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json')
+url = ("http://api.wunderground.com/api/b2d7a183c77895f0/geolocation/geolookup/conditions/q/") + str(zip) +  (".json")
+f = urllib2.urlopen(url)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
 temp_f = parsed_json['current_observation']['temp_f']
+
 print "Current temperature in %s is: %s" % (location, temp_f)
 f.close()
