433029fe76193fb217a0197fd56ef67fe57755f6
Trenton <tjboyette@gmail.com>
2015-09-24 02:06:03 -0400

    updated weather.py to check not only temp but also the weather

--- backagain.py
+++ backagain.py
@@ -1,34 +1,26 @@
 print('''
 Back again? Fantastic!
 
-What would you like me to do, ''' + name + '''?
+The time is''' + time.strftime(" %H:%M:%S (%I:%M:%S%p) on %A the %d, %B, %Y.") + '''
+      What would you like me to do?, ''' + name + '''?
 
-  "M"ath
-  "T"ell time
-  "O"pen web
+      "M"ath
+      "C"heck Current Conditions/"W"eather
+      "O"pen web
 
-Please choose a letter that corresponds with your needs.
+
+Please choose a option that corresponds with your needs.
 ''')
 
 math = {'M', 'm', 'Math', 'math', 'Mathematics', 'mathematics'}
-time = {'t', 'T', 'Time', 'time'}
+weather = {'w', 'W', 'weather', 'Weather', 'current conditions', 'Current Conditions', 'C', 'c', 'forecast', 'Forecast', 'Tell Time', 'tell Time'}
 web = {'o', 'O', 'Open web', 'Open Web', 'open web', 'open Web', 'open', 'Open'}
 
-need = None
 need = raw_input()
 
 if need in math:
-  execfile("math.py")
-if need in time:
-    #time
-	import time
-	print(time.strftime("%H:%M:%S (%I:%M:%S%p) on %A the %d, %B, %Y."))
-	print
-	print("To check if it's day or night: LOOK OUTSIDE!")
-	print
-	raw_input("Press [ENTER] to continue.")
-	need = None
-	execfile("backagain.py")
-
+  execfile('math.py')
+if need in weather:
+  execfile('weather.py')
 if need in web:
   execfile('web.py')
--- weather.py
+++ weather.py
@@ -10,12 +10,25 @@ zip = input("ZIP code? ")
 
 import urllib2
 import json
-url = ("http://api.wunderground.com/api/b2d7a183c77895f0/geolocation/geolookup/conditions/q/") + str(zip) +  (".json")
+url = ("http://api.wunderground.com/api/b2d7a183c77895f0/geolookup/conditions/q/") + str(zip) +  (".json")
 f = urllib2.urlopen(url)
 json_string = f.read()
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
 temp_f = parsed_json['current_observation']['temp_f']
+feels = parsed_json['current_observation']['feelslike_f']
+weather = parsed_json['current_observation']['weather']
+obtime = parsed_json['current_observation']['observation_time']
 
-print "Current temperature in %s is: %s" % (location, temp_f)
+print("The temperature in %s is: %s" % (location, temp_f) + ", feels like: %s" % (feels))
+
+print("Current weather in %s is: %s" % (location, weather) + ".")
+print
+print("[Note: %s" % (obtime) + "]")
+
+raw_input('''
+          press [ENTER] to continue...''')
 f.close()
+execfile('backagain.py')
+
+
