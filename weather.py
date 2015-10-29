29613601e2a5d112be8f7566be6692097795cca9
tjboyette <tjboyette@gmail.com>
2015-10-29 19:21:33 -0400

    Create weather.py
    Updated the api input

---
+++ weather.py
@@ -0,0 +1,42 @@
+#weather
+need = (None)
+
+print('''
+This function will produce for you the weather conditions for your area,
+according to Weather Underground. The next input will ask for your ZIP.''')
+
+print
+
+zip = input("   ZIP code? ")
+print
+api = raw_input("   API key for Weather Underground? ")
+print
+
+import urllib2
+import json
+url = ("http://api.wunderground.com/api/") + str(api) + ("/geolookup/conditions/q/") + str(zip) +  (".json")
+f = urllib2.urlopen(url)
+json_string = f.read()
+parsed_json = json.loads(json_string)
+pj = parsed_json['current_observation']
+loc = parsed_json['location']
+city =  loc['city'] + ", " + loc['state']
+#state = loc['state']
+temp_f = pj['temp_f']
+feels = pj['feelslike_f']
+weather = pj['weather']
+obtime = pj['observation_time']
+yes = str('yes')
+
+#print("The temperature in %s, %s is: %s" % (location, state, temp_f) + ", feels like: %s" % (feels))
+print("     The temperature in %s " % (city) + " is: %s" % (temp_f) + ", feels like: %s" % (feels))
+
+print("     Current weather in %s" % (city) + " is: %s" % (weather) + '''.
+      
+      '''+ "[Note: %s" % (obtime) + "]")
+
+raw_input('''
+          press [ENTER] to continue...''')
+f.close()
+execfile('backagain.py')
+
